64d55067201bbe823e54820c7bd2d5c408f9acd2
Aleksandr <karpeev14@ya.ru>
2023-09-24 01:48:52 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строки через запятую:");
+        string input = Console.ReadLine();
+        string[] inputStrings = input.Split(',');
+
+        string[] filteredStrings = FilterStrings(inputStrings);
+
+        Console.WriteLine("Результат фильтрации:");
+        foreach (string str in filteredStrings)
+        {
+            Console.WriteLine(str);
+        }
+    }
